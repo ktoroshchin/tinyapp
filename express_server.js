c56dd460d9e86d71c56e019c574660007cf93ae4
Konstantin Toroshchin <k.toroshchin@gmail.com>
2018-08-09 19:30:09 +0000

    Created login logout

--- express_server.js
+++ express_server.js
@@ -3,7 +3,9 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
+var cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
  function generateRandomString() {
     const characters = ['1','3','4','8','x','w','r','g','p'];
@@ -27,6 +29,8 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
                        longURL: urlDatabase[req.params.id]
@@ -39,7 +43,10 @@ app.get("/u/:shortURL",(req,res) => {
   res.redirect(urlDatabase[shortURL]);
 })
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase};
+  console.log(req.cookies["username"])
+  let templateVars = { urls: urlDatabase,
+                       username: req.cookies["username"]
+                     };
   res.render("urls_index", templateVars);
 });
 app.get("/hello", (req, res) => {
@@ -49,7 +56,8 @@ app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id]
   console.log(req.params.id)
   res.redirect("/urls")
-})
+});
+
 app.post("/urls",(req, res) => {
   var longURL = req.body.longURL;
   var shortURL = generateRandomString();
@@ -60,10 +68,21 @@ app.post("/urls",(req, res) => {
 app.post("/urls/:id",(req, res) => {
   urlDatabase[req.params.id] = req.body.longURL
   res.redirect("/urls");
-
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.post("/login",(req, res) => {
+  const username = req.body.username;  //access to the username-variable from the form/
+  res.cookie("username", username);
+  console.log(req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout",(req, res) => {
+  res.clearCookie("username")
+  res.redirect("/urls");
+})
 generateRandomString();
